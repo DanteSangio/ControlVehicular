ee5b9ecc1687a93f409206e77163c0136ce47543
Fede <Fede@192.168.2.27>
2018-09-18 18:14:50 -0300

    Comm

--- Example/src/FRTOS.c
+++ Example/src/FRTOS.c
@@ -37,10 +37,10 @@
 #define UART_SELECTION 	LPC_UART1
 #define IRQ_SELECTION 	UART1_IRQn
 #define HANDLER_NAME 	UART1_IRQHandler
-#define TXD1	0,15	//RX UART1	(Pin 13 LPC1769) (EXP 18)
-#define	RXD1	0,16	//TX UART1	(Pin 14 LPC1769) (EXP 19)
-#define TXD2	0,10	//RX UART2	(Pin 40 LPC1769)
-#define RXD2	0,11	//TX UART2	(Pin 41 LPC1769)
+#define TXD1	0,15	//RX UART1	(Pin 14 LPC1769) (EXP 18)
+#define	RXD1	0,16	//TX UART1	(Pin 13 LPC1769) (EXP 19)
+#define TXD2	0,10	//RX UART2	(Pin 41 LPC1769)
+#define RXD2	0,11	//TX UART2	(Pin 40 LPC1769)
 
 #define UART_SRB_SIZE 32	//S:Send - Transmit ring buffer size
 #define UART_RRB_SIZE 1024	//R:Receive - Receive ring buffer size
@@ -235,6 +235,7 @@ static void xTaskUART1Config(void *pvParameters)
 /* vTaskGSMConfig */
 static void vTaskGSMConfig(void *pvParameters)
 {
+	/*
 	Chip_GPIO_SetPinOutHigh(LPC_GPIO, RST_GSM);
 	vTaskDelay(10/portTICK_RATE_MS);	//Espero 10ms
 	Chip_GPIO_SetPinOutLow(LPC_GPIO, RST_GSM);
@@ -254,6 +255,13 @@ static void vTaskGSMConfig(void *pvParameters)
 
 	//Seguir ejemplo de https://www.teachmemicro.com/send-data-sim800-gprs-thingspeak/
 	//Utilizando libreria Adafruit_FONA
+	*/
+
+
+	/*
+	 * Pagina 26 Hardware Design
+	 */
+
 
 	/*
 	 * Pagina 208 Command Manual
