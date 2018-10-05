7d6ad2424a540811cf496f9530a226102f6bb0f9
Dante <33044411+DanteSangio@users.noreply.github.com>
2018-10-05 20:20:16 -0300

    para probar

--- Example/src/main.c
+++ Example/src/main.c
@@ -11,7 +11,8 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "semphr.h"
-
+#include "MFRC522.h"
+#include "rfid_utils.h"
 
 
 
@@ -19,10 +20,11 @@
 
 
 
+#define DEBUGOUT1(...) printf(__VA_ARGS__)
 #define DEBUGOUT(...) printf(__VA_ARGS__)
 #define DEBUGSTR(...) printf(__VA_ARGS__)
 
-
+#define MIN_BALANCE 300
 
 
 /*****************************************************************************
@@ -33,16 +35,22 @@ static volatile bool fIntervalReached;
 static volatile bool fAlarmTimeMatched;
 static volatile bool On0, On1;
 
+// RFID structs
+MFRC522Ptr_t mfrcInstance;
+
+int last_balance = 0;
+unsigned int last_user_ID;
+
 /*****************************************************************************
  * Public types/enumerations/variables
  ****************************************************************************/
 
+SemaphoreHandle_t Semaforo_RFID;
 SemaphoreHandle_t Semaforo_RTC;
 
 /*****************************************************************************
  * Private functions
  ****************************************************************************/
-
 /* Gets and shows the current time and date */
 static void showTime(RTC_TIME_T *pTime)
 {
@@ -54,14 +62,42 @@ static void showTime(RTC_TIME_T *pTime)
 			 pTime->time[RTC_TIMETYPE_YEAR]);
 }
 
-/*****************************************************************************
- * Public functions
- ****************************************************************************/
+
+/*******************************************************************************
+ *  System routine functions
+ ******************************************************************************/
 
 /**
- * @brief	RTC interrupt handler
- * @return	Nothing
+ * Executed every time the card reader detects a user in
  */
+void userTapIn()
+
+{
+
+//	show card UID
+	DEBUGOUT("\nCard uid bytes: ");
+	for (uint8_t i = 0; i < mfrcInstance->uid.size; i++) {
+		DEBUGOUT(" %X", mfrcInstance->uid.uidByte[i]);
+	}
+	DEBUGOUT("\n\r");
+
+
+	// Convert the uid bytes to an integer, byte[0] is the MSB
+	last_user_ID =
+		(int)mfrcInstance->uid.uidByte[3] |
+		(int)mfrcInstance->uid.uidByte[2] << 8 |
+		(int)mfrcInstance->uid.uidByte[1] << 16 |
+		(int)mfrcInstance->uid.uidByte[0] << 24;
+
+	DEBUGOUT("\nCard Read user ID: %u \n\r",last_user_ID);
+
+	Comparar(last_user_ID);
+
+	// Read the user balance NO BORRAR SINO NO DETECTA CUANDO HAY TARJETA NUEVA
+	last_balance = readCardBalance(mfrcInstance);
+
+}
+
 void RTC_IRQHandler(void)
 {
 
@@ -77,6 +113,26 @@ void RTC_IRQHandler(void)
 	}
 }
 
+
+/*****************************************************************************
+ * Public functions
+ ****************************************************************************/
+
+
+static void xTaskRFIDConfig(void *pvParameters)
+{
+	SystemCoreClockUpdate();
+
+	DEBUGOUT1("PRUEBA RFID..\n");	//Imprimo en la consola
+
+	setupRFID(&mfrcInstance);
+
+	xSemaphoreGive(Semaforo_RFID);//Doy el semaforo para que se inicie la lectura
+
+	vTaskDelete(NULL);	//Borra la tarea
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* vTaskRTConfig */
 static void xTaskRTConfig(void *pvParameters)
 {
 	RTC_TIME_T FullTime;
@@ -120,6 +176,30 @@ static void xTaskRTConfig(void *pvParameters)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* vTaskInicTimer */
+static void vTaskRFID(void *pvParameters)
+{
+	xSemaphoreTake(Semaforo_RFID, portMAX_DELAY); //semaforo de inicializacion de rfid
+	while (1)
+	{
+
+		// Look for new cards in RFID2
+		if (PICC_IsNewCardPresent(mfrcInstance))
+		{
+			// Select one of the cards
+			if (PICC_ReadCardSerial(mfrcInstance))
+			{
+//				int status = writeCardBalance(mfrcInstance, 100000); // used to recharge the card
+				 userTapIn();
+			}
+		}
+		vTaskDelay( 1000 / portTICK_PERIOD_MS );//Muestreo cada 1 seg
+	}
+
+	vTaskDelete(NULL);	//Borra la tarea si sale del while
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* vTaskRTC */
 static void vTaskRTC(void *pvParameters)
 {
 	RTC_TIME_T FullTime;
@@ -131,7 +211,6 @@ static void vTaskRTC(void *pvParameters)
 	}
 	vTaskDelete(NULL);	//Borra la tarea si sale del while
 }
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* main
 */
@@ -139,15 +218,28 @@ int main(void)
 {
 	SystemCoreClockUpdate();
 
+	/* Initializes GPIO */
+	Chip_GPIO_Init(LPC_GPIO);
+
+	vSemaphoreCreateBinary(Semaforo_RFID);
+	xSemaphoreTake(Semaforo_RFID, portMAX_DELAY); //semaforo de inicializacion de rfid
 	vSemaphoreCreateBinary(Semaforo_RTC);			//Creamos el semaforo
+
+	xTaskCreate(vTaskRFID, (char *) "vTaskRFID",
+				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
+				(xTaskHandle *) NULL);
+
+	xTaskCreate(xTaskRFIDConfig, (char *) "xTaskRFIDConfig",
+				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 2UL),
+				(xTaskHandle *) NULL);
 
 	xTaskCreate(vTaskRTC, (char *) "vTaskRTC",
 				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
 				(xTaskHandle *) NULL);
+
 	xTaskCreate(xTaskRTConfig, (char *) "xTaskRTConfig",
 				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 2UL),
 				(xTaskHandle *) NULL);
-
 
 	/* Start the scheduler */
 	vTaskStartScheduler();
